53d7b1ce3ed67d9180ef3953de58471ecbbd37cf
domanskyi <bruce3773@gmail.com>
2021-03-25 23:41:19 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useFrog } from "./api";
 
 function App() {
+  const { data, loading } = useFrog();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!loading && (
+          <img
+            src={data}
+            className="App-logo"
+            alt="logo"
+            style={{
+              maxWidth: 300,
+              height: "100%",
+            }}
+          />
+        )}
+        <p>Legush'ka</p>
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,16 @@
+import useSWR from "swr";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API;
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+export const useFrog = () => {
+  const { data, error } = useSWR(API_URL, fetcher);
+
+  return {
+    data,
+    error,
+    loading: !data && !error,
+  };
+};
